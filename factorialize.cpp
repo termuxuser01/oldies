875d37c1cdf9bc66bd90e193496b3a4ef0084a4b
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2019-07-31 15:59:34 -0500

    Create factorialize.cpp

---
+++ factorialize.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int fact, i;
+	cout<<"enter a number to factorialize\n";
+	cin>>fact;
+	int r = fact;
+	
+	
+	for(i = 1; i < fact; i++)
+	{
+		r *= i;
+	}
+	cout<<"result is "<<r<<"\n";
+	return 0;
+}
